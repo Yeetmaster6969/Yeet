88aa106e4fb8867f32301355e6e94be6ac42e956
aceful <47220088+TheAceful@users.noreply.github.com>
2020-02-06 10:25:05 +0200

    Revert "asda"
    This reverts commit 2c74bb5663b7fd0538f5e161b2dc73abf6c7d528.

--- lib/src/calendar.dart
+++ lib/src/calendar.dart
@@ -1,12 +1,6 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:material_test/src/app.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:firebase/firebase.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 
 class SecondPage extends StatefulWidget{
@@ -21,21 +15,17 @@ class SecondPageState extends State <SecondPage> {
 
 CalendarController _controller;
 
-//NEW STUFF
-String todoName = "Display the todo name here";
+void initState(){
 
-@override
-void initState() {
-FirebaseTodos.getTodo("info1").then(updateTodo);
 super.initState();
 _controller = CalendarController();
   
   }
-  @override
+  
    Widget build(context){
      return new Scaffold (
        backgroundColor: bgColor,
-       appBar: new AppBar(title: new Text("$todoName"),
+       appBar: new AppBar(title: new Text('Calendar'),
        backgroundColor: barColor,
        ),
      body: new Container(
@@ -59,85 +49,5 @@ _controller = CalendarController();
        
      ),
      );
-  }
-
-    updateTodo(Todo value) {
-    var name = value.name;
-    setState(() {
-      todoName = name;
-    });
-  }
-  
-}
-
-//NEW STUFF
-class Todo {
-  final String key;
-  String name;
-
-  Todo.fromJson(this.key, Map data) {
-    name = data[ "name"];
-    if(name == null) {
-      name = "";
-    } 
-  }
-}
-
-class FirebaseTodos {
-  static Future<StreamSubscription<Event>> getTodoStream(String todoKey,
-      void onData(Todo todo)) async {
-    String accountKey = await Preferences.getAccountKey();
-
-    StreamSubscription<Event> subscription = FirebaseDatabase.instance
-        .reference()
-        .child("users")
-        .child(accountKey)
-        .child("userinfo")
-        .child(todoKey)
-        .onValue
-        .listen((Event event) {
-      var todo = new Todo.fromJson(event.snapshot.key, event.snapshot.value);
-      onData(todo);
-    });
-      return subscription;
-  }
-
-    static Future<Todo> getTodo(String todoKey) async {
-      Completer<Todo> completer = new Completer<Todo>();
-
-      String accountKey = await Preferences.getAccountKey();
-
-      FirebaseDatabase.instance
-          .reference()
-          .child("users")
-          .child(accountKey)
-          .child("userinfo")
-          .child(todoKey)
-          .once()
-          .then((DataSnapshot snapshot) {
-        var todo = new Todo.fromJson(snapshot.key, snapshot.value);
-        completer.complete(todo);
-      });
-      return completer.future;
-    }
-}
-
-class Preferences {
-  static const String ACCOUNT_KEY = "accountKey";
-
-  static Future<bool> setAccountKey(String accountKey) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString(ACCOUNT_KEY, accountKey);
-    return prefs.commit();
-  }
-
-  static Future<String> getAccountKey() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String accountKey = prefs.getString(ACCOUNT_KEY);
-
-    if(accountKey == null){
-      accountKey = "0";
-    }
-    return accountKey;
-  }
+   }
 }
